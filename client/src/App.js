a550ed0e0f9b95c97a064707f8d47c1bc5e86ed9
Jason Biggs <jason@thinkbigg.dev>
2024-04-09 20:37:10 -0500

    reworked the app.js file to utilize react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Navbar'
 import React from 'react';
 import Home from './Home';
 import AddAShow from './AddAShow';
 import WatchAShow from './WatchAShow';
 
-function App() {
 
+function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <h1>Randomize Series Episode</h1>
-        {/* <Home /> */}
-        {/* <AddAShow /> */}
-        <WatchAShow />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <h1>Randomize Series Episode</h1>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="AddAShow" element={<AddAShow />}/>
+            <Route path="WatchAShow" element={<WatchAShow />}/>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
