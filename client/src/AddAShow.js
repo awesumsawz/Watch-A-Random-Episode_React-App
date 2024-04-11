ece691ac9ed094bed31d18b3c91b3a4820b7ce4f
Jason Biggs <jason@thinkbigg.dev>
2024-04-11 13:12:17 -0500

    moved the season wrapper and content into its own component AND move dhte addASeaon button into that component

--- client/src/AddAShow.js
+++ client/src/AddAShow.js
@@ -1,65 +1,14 @@
 import SaveSeriesName from "./AddAShowComponents/SaveSeriesName";
+import SeasonBuilder from "./AddAShowComponents/SeasonBuilder";
+
 const AddAShow = () => {
 	return (
 		<div className="addAShow">
 			<h2>Add A Series</h2>
 			<div className="container viewOne">
-				<div className="wrapper">
-					<div className="seasons">
-						<div className="left">
-							<span className="season">Season 1</span>
-						</div>
-						<div className="right static">
-							<span>23 episodes</span>
-							<button className="editTrigger">Edit</button>
-						</div>
-					</div>
-					<div className="seasons">
-						<div className="left">
-							<span className="season">Season 2</span>
-						</div>
-						<div className="right edit">
-							<div className="seasonEpisodes">
-								<span>How Many Episodes in Season 2?</span>
-								<input type="text" />
-							</div>
-							<button className="addEpisodesButton">Save Episodes</button>
-						</div>
-					</div>
-					<div className="seasons">
-						<div className="left">
-							<span className="season">Season 3</span>
-						</div>
-						<div className="right edit">
-							<div className="seasonEpisodes">
-								<span>How Many Episodes in Season 3?</span>
-								<input type="text" />
-							</div>
-							<button className="addEpisodesButton">Save Episodes</button>
-						</div>
-					</div>
-					<div className="seasons">
-						<div className="left">
-							<span className="season">Season 4</span>
-						</div>
-						<div className="right static">
-							<span>23 episodes</span>
-							<button className="editTrigger">Edit</button>
-						</div>
-					</div>
-					<div className="seasons">
-						<div className="left">
-							<span className="season">Season 5</span>
-						</div>
-						<div className="right static">
-							<span>23 episodes</span>
-							<button className="editTrigger">Edit</button>
-						</div>
-					</div>
-				</div>
 				<SaveSeriesName />
+				<SeasonBuilder />
 				<div className="buttons">
-					<button id="addASeasonButton">+ Add A Season</button>
 					<button id="saveSeriesButton">Save Series</button>
 				</div>
 			</div>
