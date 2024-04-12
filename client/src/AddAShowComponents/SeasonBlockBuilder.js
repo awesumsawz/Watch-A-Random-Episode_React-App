28f9edfc0355d9afb041085c4ccb5914c8adf79d
Jason Biggs <jason@thinkbigg.dev>
2024-04-12 14:17:46 -0500

    updated the season block builder in order to toggle the edit/save views of each block

--- client/src/AddAShowComponents/SeasonBlockBuilder.js
+++ client/src/AddAShowComponents/SeasonBlockBuilder.js
@@ -1,41 +1,46 @@
+import React, { useState } from 'react';
+
 function SeasonBlockBuilder({ seasons }) {
+    const [editModes, setEditModes] = useState({});
 
-	function toggleEdit() {
-
-	}
-
-
+    function toggleEdit(seasonId) {
+        setEditModes(prevModes => ({
+            ...prevModes,
+            [seasonId]: !prevModes[seasonId]
+        }));
+    }
 
     const seasonBlocks = Object.entries(seasons).map(([key, season]) => {
-        
-		const seasonNumber = season.id;
+        const seasonNumber = season.id;
         const episodeCount = Object.entries(season.episodes).length;
+        const isEditing = editModes[seasonNumber];
+
         return (
             <div className="seasons" key={seasonNumber}>
-
                 <div className="left">
                     <span className="season">Season {seasonNumber}</span>
                 </div>
-
-                <div className="right static">
-                    <span>{episodeCount} episodes</span>
-                    <button className="editTrigger" onClick={toggleEdit(this)}>Edit</button>
+                <div className={`right ${isEditing ? 'edit' : 'static'}`}>
+                    {isEditing ? (
+                        <>
+                            <div className="seasonEpisodes">
+                                <input type="text" defaultValue={episodeCount} />
+                                <span>episodes</span>
+                            </div>
+                            <button className="saveTrigger" onClick={() => toggleEdit(seasonNumber)}>Save</button>
+                        </>
+                    ) : (
+                        <>
+                            <span>{episodeCount} episodes</span>
+                            <button className="editTrigger" onClick={() => toggleEdit(seasonNumber)}>Edit</button>
+                        </>
+                    )}
                 </div>
-
-				<div className="right edit hidden">
-					<div className="seasonEpisodes">
-						<input type="text" placeholder={episodeCount} />
-						<span>episodes</span>
-					</div>
-					<button className="saveTrigger" onClick={toggleEdit(this)}>Save</button>
-				</div>
-
             </div>
         );
-
     });
 
-    return seasonBlocks;
+    return <>{seasonBlocks}</>;
 }
 
 export default SeasonBlockBuilder;
